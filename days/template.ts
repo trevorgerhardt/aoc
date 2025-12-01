8f8cff7e4680f8b6d8de27791d01ad9f36e67398
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-12-01 13:50:09 +0800

    Update template.ts

--- days/template.ts
+++ days/template.ts
@@ -1,15 +1,13 @@
 export const exampleInput1 = ""
-
-export const exampleInput2 = ""
-
+export const exampleResult1 = 0
 export const result1 = 0
-
-export const result2 = 0
-
 export const getResult1 = (input: typeof exampleInput1) => {
 	return input
 }
 
+export const exampleInput2 = exampleInput1
+export const exampleResult2 = 0
+export const result2 = 0
 export const getResult2 = (input: typeof exampleInput2) => {
 	return input
 }
