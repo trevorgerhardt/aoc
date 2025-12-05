6efecaa9db33eb39b64611bc935597b71e4be5eb
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-12-05 14:59:44 +0800

    Update create.ts

--- create.ts
+++ create.ts
@@ -1,6 +1,9 @@
 import { getInput } from "./utils";
 
 const TODAY = new Date().getDate();
+const TODAY_PATH = `days/${TODAY}.ts`;
+if (await Bun.file(TODAY_PATH).exists())
+  throw Error(`${TODAY_PATH} already exists. Will not overwrite.`);
 console.log(`Creating new day: ${TODAY}`);
 const templateText = await Bun.file(`days/template.ts`).text();
 const dayText = templateText.replace(`"DAY"`, String(TODAY));
