067e7732d2ab504fbb1f8e32083a413efc576495
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-12-07 07:49:56 +0800

    Update template

--- days/template.ts
+++ days/template.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "bun:test";
+import { test, expect } from "bun:test";
 import { getInput } from "../utils";
 import type { Part } from "../types";
 
@@ -8,7 +8,7 @@ export const P1: Part = {
   exampleInput: ``,
   exampleResult: 0,
   result: 0,
-  getResult: (input) => {
+  getResult: (input: string) => {
     return 0;
   },
 };
@@ -17,30 +17,24 @@ export const P2: Part = {
   exampleInput: P1.exampleInput,
   exampleResult: 0,
   result: 0,
-  getResult: (input) => {
+  getResult: (input: string) => {
     return 0;
   },
 };
 
-describe(`day ${DAY}`, async () => {
-  const input = await getInput(DAY);
-  describe("pt1", () => {
-    it("example", () => {
-      expect(P1.getResult(P1.exampleInput)).toBe(P1.exampleResult);
-    });
+const input = await getInput(DAY);
+test("pt1 example", () => {
+  expect(P1.getResult(P1.exampleInput)).toBe(P1.exampleResult);
+});
 
-    it("input", () => {
-      expect(P1.getResult(input)).toBe(P1.result);
-    });
-  });
+test("pt1", () => {
+  expect(P1.getResult(input)).toBe(P1.result);
+});
 
-  describe("pt2", () => {
-    it("example", () => {
-      expect(P2.getResult(P2.exampleInput)).toBe(P2.exampleResult);
-    });
+test("pt2 example", () => {
+  expect(P2.getResult(P2.exampleInput)).toBe(P2.exampleResult);
+});
 
-    it("input", () => {
-      expect(P2.getResult(input)).toBe(P2.result);
-    });
-  });
+test("pt2", () => {
+  expect(P2.getResult(input)).toBe(P2.result);
 });
